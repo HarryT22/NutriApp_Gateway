34ee298a5011a32e5ffecbf654e734d160860406
ht900929 <ht900929@fh-muenster.de>
2022-06-02 18:34:04 +0200

    first gateway push

---
+++ src/main/java/com/gateway/nutriappgateway/NutriAppGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.nutriappgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriAppGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutriAppGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gateway/nutriappgateway/NutriAppGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.nutriappgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriAppGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
